6246fa197dfc2ffcab76a32fdbe1e2870e5d3dc0
wwc <onechoo1106@gmail.com>
2023-04-25 18:19:08 +1000

    Initial Commit

---
+++ Assignment2/Assignment2App.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment2App.swift
+//  Assignment2
+//
+//  Created by 최원우 on 25/4/2023.
+//
+
+import SwiftUI
+
+@main
+struct Assignment2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
